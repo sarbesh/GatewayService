1c3a8d96cebe706f5650fbcae3eb8d78e82dc1f8
sarbesh <sarbeshsarkarsks@gmail.com>
2021-09-23 20:01:29 +0530

    Gateway files

--- src/test/java/com/sarbesh/webapp/GatewayServiceApplicationTests.java
+++ src/test/java/com/sarbesh/webapp/GatewayServiceApplicationTests.java
@@ -1,16 +1,13 @@
 package com.sarbesh.webapp;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class GatewayServiceApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 	}
 
 }
